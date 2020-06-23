833e561fdb666a933a9c292c5aad491a3818a1f4
Discode Stu <dstuart85@yahoo.com>
2020-06-23 13:37:37 -0400

    added SET_RESULTS_POSTS reducer

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,10 +1,10 @@
 import {
-    SET_RECENT_POSTS
+    SET_RECENT_POSTS, SET_RESULTS_POSTS
 } from '../actions/types';
 import action from 'redux-form/lib/actions';
 
 const INIT_STATE = { 
-    posts : [],
+    resultsPosts : [],
     recentPosts : []
 }
 
@@ -15,7 +15,15 @@ export default function(state = INIT_STATE, action) {
             return {
                 ...state, 
                 recentPosts
-            };
+            }
+
+        case SET_RESULTS_POSTS:
+            const resultsPosts = action.payload;
+            return{
+                ...state,
+                resultsPosts
+            }
+
         default:
             return state;
     }
